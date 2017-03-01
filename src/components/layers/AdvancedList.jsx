d691a04a2c9122985a81a069a3f2b56c70aff41d
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-03-01 18:36:27 +0100

    Timeline theme

--- src/components/layers/AdvancedList.jsx
+++ src/components/layers/AdvancedList.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Timeline } from 'aqueduct-components';
+
+const points = [
+  {
+    label: '',
+    value: '1'
+  },
+  {
+    label: '',
+    value: '2'
+  },
+  {
+    label: '',
+    value: '3'
+  },
+  {
+    label: '',
+    value: '4'
+  },
+  {
+    label: '',
+    value: '5'
+  }
+];
 
 export default function AdvancedList() {
-  return <div>Advanced list</div>;
+  return (
+    <Timeline className="-rate -bloqued" items={points} selected={{ value: '3' }} onChange={() => {}} />
+  );
 }
 
 AdvancedList.propTypes = {};

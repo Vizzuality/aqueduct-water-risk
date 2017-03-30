f771518ba41f7668aafc800094c86eb1f9945d12
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-03-30 15:41:42 +0200

    Collapsable legend

--- src/components/legend/Legend.jsx
+++ src/components/legend/Legend.jsx
@@ -2,30 +2,60 @@ import React from 'react';
 import ChoroplethItem from './ChoroplethItem';
 import startCase from 'lodash/startCase';
 import upperFirst from 'lodash/upperFirst';
+import { Icon } from 'aqueduct-components';
 
 const legendItems = {
   choropleth: ChoroplethItem
 };
 
-export default function Legend({ layers, config }) {
-  const indicator = config.layers.active[0];
-  let name = upperFirst(startCase(indicator));
-  name += indicator === 'water_stress' ? ` - ${upperFirst(config.ponderation.scheme)}` : '';
+export default class Legend extends React.Component {
 
-  return (
-    <section className="c-legend">
-      <div className="legend-content">
-        {layers.map((layer, index) => {
-          const LegendItem = legendItems[layer.legendConfig.type];
-          return (
-            <li key={index}>
-              {LegendItem ? <LegendItem layer={layer} name={name} /> : null}
-            </li>
-          );
-        })}
-      </div>
-    </section>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      closed: false
+    };
+
+    // Bindings
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      closed: !this.state.closed
+    });
+  }
+
+  render() {
+    const { layers, config } = this.props;
+
+    const indicator = config.layers.active[0];
+    const name = upperFirst(startCase(indicator));
+
+    return (
+      <section className="c-legend">
+        <header className="legend-header">
+          <h1 className="legend-title">legend</h1>
+          <button onClick={this.toggle} className="header-btn" type="button">
+            <Icon name={`icon-arrow-${this.state.closed ? 'down' : 'up'}-2`} className="header-icon" />
+          </button>
+        </header>
+        {this.state.closed ||
+          <div className="legend-content">
+            {layers.map((layer, index) => {
+              const LegendItem = legendItems[layer.legendConfig.type];
+              return (
+                <li key={index}>
+                  {LegendItem ? <LegendItem layer={layer} name={name} /> : null}
+                </li>
+              );
+            })}
+          </div>
+        }
+      </section>
+    );
+  }
 }
 
 Legend.propTypes = {

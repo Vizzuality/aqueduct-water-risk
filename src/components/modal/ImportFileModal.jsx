d4ba35151ad3a9cc49766e9a7d25fa8f76afc4a3
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-05-11 13:03:15 +0200

    Import modal segmented ui

--- src/components/modal/ImportFileModal.jsx
+++ src/components/modal/ImportFileModal.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import { SegmentedUi } from 'aqueduct-components';
 
 export default class ImportFileModal extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tab: 'csv'
+    };
+
+    this.triggerChangeTab = this.triggerChangeTab.bind(this);
+  }
+
+  /**
+   * UI EVENTS
+   * - triggerChangeTab
+  */
+  triggerChangeTab(selected) {
+    this.setState({
+      tab: selected.value
+    });
+  }
+
   render() {
     return (
       <div className="c-import">
-        <h1>Import file!</h1>
+        <SegmentedUi
+          className="-tabs-light"
+          items={[{
+            label: 'CSV',
+            value: 'csv'
+          }, {
+            label: 'JSON',
+            value: 'json'
+          }, {
+            label: 'Shape file',
+            value: 'shape_file'
+          }, {
+            label: 'XLXS',
+            value: 'xlxs'
+          }]}
+          selected={this.state.tab}
+          onChange={this.triggerChangeTab}
+        />
       </div>
     );
   }

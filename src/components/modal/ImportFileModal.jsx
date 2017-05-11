2b12405add88564dafa5085603076d1bde95eeb7
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-05-11 18:01:11 +0200

    Import: modal converting correct files

--- src/components/modal/ImportFileModal.jsx
+++ src/components/modal/ImportFileModal.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { SegmentedUi } from 'aqueduct-components';
+import Dropzone from 'react-dropzone';
+import { Spinner, post, toggleModal } from 'aqueduct-components';
+import { dispatch } from 'main';
+import { setPoints } from 'modules/analyzeLocations';
 
 export default class ImportFileModal extends React.Component {
 
@@ -7,43 +10,147 @@ export default class ImportFileModal extends React.Component {
     super(props);
 
     this.state = {
-      tab: 'csv'
+      accepted: [],
+      rejected: [],
+      dropzoneActive: false,
+      loading: false,
+      error: null
     };
 
-    this.triggerChangeTab = this.triggerChangeTab.bind(this);
+    this.triggerOpenDialog = this.triggerOpenDialog.bind(this);
+    this.onDragEnter = this.onDragEnter.bind(this);
+    this.onDragLeave = this.onDragLeave.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  /**
+   * DROPZONE EVENTS
+   * - onDragEnter
+   * - onDragLeave
+   * - onDrop
+  */
+  onDragEnter() {
+    this.setState({
+      dropzoneActive: true
+    });
+  }
+
+  onDragLeave() {
+    this.setState({
+      dropzoneActive: false
+    });
+  }
+
+  onDrop(accepted, rejected) {
+    this.setState({
+      accepted,
+      rejected,
+      dropzoneActive: false
+    }, () => {
+      if (this.state.accepted.length) {
+        this.convertFile(this.state.accepted[0]);
+      }
+    });
+  }
+
+  /**
+   * HELPERS
+   * - getFileName
+   * - convertFile
+  */
+
+  getFileName() {
+    const { accepted } = this.state;
+
+    if (accepted.length) {
+      const current = accepted[0];
+      return current.name;
+    }
+
+    return 'Select file to import data';
+  }
+
+  convertFile(file) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    this.setState({ loading: true, error: null });
+
+    post({
+      type: 'POST',
+      url: `${config.API_URL}/ogr/convert`,
+      body: formData,
+      multipart: true,
+      onSuccess: (response) => {
+        this.setState({ loading: false });
+        // dispatch(setPoints(response.data.attributes.features, {}));
+        dispatch(toggleModal(false, {}));
+      },
+      onError: (err) => {
+        this.setState({ error: err, loading: false });
+      }
+    });
   }
 
   /**
    * UI EVENTS
-   * - triggerChangeTab
+   * - triggerOpenDialog
   */
-  triggerChangeTab(selected) {
-    this.setState({
-      tab: selected.value
-    });
+  triggerOpenDialog() {
+    this.dropzone.open();
   }
 
   render() {
+    const { dropzoneActive, loading } = this.state;
     return (
-      <div className="c-import">
-        <SegmentedUi
-          className="-tabs-light"
-          items={[{
-            label: 'CSV',
-            value: 'csv'
-          }, {
-            label: 'JSON',
-            value: 'json'
-          }, {
-            label: 'Shape file',
-            value: 'shape_file'
-          }, {
-            label: 'XLXS',
-            value: 'xlxs'
-          }]}
-          selected={this.state.tab}
-          onChange={this.triggerChangeTab}
-        />
+      <div className="c-import-modal">
+        <Spinner isLoading={loading} />
+
+        <Dropzone
+          ref={(node) => { this.dropzone = node; }}
+          className="c-dropzone"
+          activeClassName="-active"
+          rejectClassName="-reject"
+          multiple={false}
+          disableClick
+          disablePreview
+          onDrop={this.onDrop}
+          onDragEnter={this.onDragEnter}
+          onDragLeave={this.onDragLeave}
+        >
+          {dropzoneActive &&
+            <div className="dropzone-active">
+              <h2>Drop files...</h2>
+            </div>
+          }
+
+          <header className="dropzone-header">
+            <h2>Import files</h2>
+            {/* <p>Drop a file in the designated area to analyze or subscribe to it. The recommended maximum file size is 1MB. Anything larger than that may not work properly.</p>
+            <p>NOTE: Only point data is supported, not polygon and line data. Please ensure that your file only contains point data.</p> */}
+
+            <p>List of supported file formats:</p>
+            <ul>
+              <li>Unzipped: .csv, .json, .geojson, .kml, .kmz (.csv files must contain a geom column that contains geographic information)</li>
+              <li>Zipped: .shp (zipped shapefiles must include .shp, .shx, .dbf, and .prj files)</li>
+            </ul>
+          </header>
+
+          <div className="dropzone-file">
+            <div
+              className="dropzone-file-name"
+              // onClick={this.triggerOpenDialog} TODO: Yes or no?
+            >
+              {this.getFileName()}
+            </div>
+            <button
+              className="c-btn -primary -light"
+              onClick={this.triggerOpenDialog}
+            >
+              Select file
+            </button>
+          </div>
+        </Dropzone>
       </div>
     );
   }

bbed31e7ae250c6bab8b04f0aa9e2f6c06d03991
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-02-14 16:05:25 +0100

    Removes map & app containers

---
+++ src/components/pages/Map/index.js
@@ -0,0 +1,54 @@
+import { connect } from 'react-redux';
+// TO-DO: remove this.
+import { store } from 'main';
+
+// actions
+import { getLayers } from 'modules/layers/actions';
+import { updateUrl } from 'modules/url';
+import { setScope } from 'modules/scope';
+import { setPoints, setSelectedPoints, setAnalysis, saveOnGeostore } from 'modules/analyzeLocations';
+
+// selectors
+import getActiveLayers from 'selectors/layers_active';
+import getCategorizedPoints from 'selectors/points_categorized';
+
+// component
+import MapPage from './component';
+
+export default connect(
+  state => ({
+    scope: state.scope.name,
+    mapState: state.map,
+    loading: state.layers.loading,
+    mapView: state.mapView,
+    filters: state.mapView.filters,
+    ponderation: state.mapView.ponderation,
+    analyzeLocations: state.analyzeLocations,
+    layersActive: [] || getActiveLayers(state),
+    pointsCategorized: getCategorizedPoints(state)
+  }),
+  dispatch => ({
+    updateUrl() { dispatch(updateUrl()); },
+    setScope(scope) { dispatch(setScope(scope)); },
+    setPoints(points) {
+      dispatch(setPoints(points));
+      dispatch(saveOnGeostore(points));
+    },
+    setSelectedPoints(active) { dispatch(setSelectedPoints(active)); },
+    addPoint(point) {
+      const points = store.getState().analyzeLocations.points.list.slice();
+      points.push(point);
+      dispatch(setPoints(points));
+      dispatch(saveOnGeostore(points));
+    },
+    removePoint(id) {
+      const points = store.getState().analyzeLocations.points.list.slice();
+      const pointIndex = points.findIndex(point => point.id === id);
+      points.splice(pointIndex, 1);
+      dispatch(setPoints(points));
+      dispatch(saveOnGeostore(points));
+    },
+    setAnalysis(weights, points) { dispatch(setAnalysis(weights, points)); },
+    getLayers() { dispatch(getLayers()); }
+  })
+)(MapPage);
--- src/containers/app/App.js
+++
@@ -1,3 +0,0 @@
-import App from 'components/app/App';
-
-export default App;

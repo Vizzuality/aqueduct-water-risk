8bbc6c624d6b404797c12979d3c6ef8ef6ebdfcb
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-02-25 15:28:48 +0100

    Fixes code regression

--- src/components/pages/map/index.js
+++ src/components/pages/map/index.js
@@ -5,7 +5,7 @@ import { store } from 'main';
 // actions
 import { getLayers } from 'modules/layers/actions';
 import { setScope, updateUrl } from 'modules/app/actions';
-import { setPoints, setSelectedPoints, setAnalysis, saveOnGeostore } from 'modules/analyze-locations-tab/actions';
+import { setPoints, setSelectedPoints, setAnalysis, onSaveGeostore } from 'modules/analyze-locations-tab/actions';
 
 // selectors
 import getCategorizedPoints from 'selectors/points_categorized';
@@ -29,21 +29,21 @@ export default connect(
     setScope(scope) { dispatch(setScope(scope)); },
     setPoints(points) {
       dispatch(setPoints(points));
-      dispatch(saveOnGeostore(points));
+      dispatch(onSaveGeostore(points));
     },
     setSelectedPoints(active) { dispatch(setSelectedPoints(active)); },
     addPoint(point) {
       const points = store.getState().analyzeLocations.points.list.slice();
       points.push(point);
       dispatch(setPoints(points));
-      dispatch(saveOnGeostore(points));
+      dispatch(onSaveGeostore(points));
     },
     removePoint(id) {
       const points = store.getState().analyzeLocations.points.list.slice();
       const pointIndex = points.findIndex(point => point.id === id);
       points.splice(pointIndex, 1);
       dispatch(setPoints(points));
-      dispatch(saveOnGeostore(points));
+      dispatch(onSaveGeostore(points));
     },
     setAnalysis(weights, points) { dispatch(setAnalysis(weights, points)); },
     getLayers() { dispatch(getLayers()); }

412963b92680b24ab4fdf203c4a800bbae348206
Andrés González <andres.gonzalez@vizzuality.com>
2019-07-29 10:45:20 +0200

    Fixes future indicator on change measurement

--- src/components/future-tab/indicator-list/selectors.js
+++ src/components/future-tab/indicator-list/selectors.js
@@ -1,22 +1,13 @@
 import { createSelector } from 'reselect';
 
-import { FUTURE_INDICATORS, DEFAULT_FUTURE_INDICATOR } from 'constants/indicators';
+import { FUTURE_INDICATORS } from 'constants/indicators';
 
 // states
-const getCurrentIndicator = state => state.settings.filters.indicator;
 const getProjection = state => state.settings.filters.projection;
-
-export const setCurrentIndicator = createSelector(
-  [getCurrentIndicator, getProjection],
-  (_currentIndicator, _projection) => FUTURE_INDICATORS[_projection].includes(_currentIndicator) ? _currentIndicator : DEFAULT_FUTURE_INDICATOR[_projection]
-);
 
 export const getIndicators = createSelector(
   [getProjection],
   _projection => FUTURE_INDICATORS[_projection].map(_indicator => ({ label: _indicator.name, value: _indicator.id }))
 );
 
-export default {
-  setCurrentIndicator,
-  getIndicators
-};
+export default { getIndicators };
